7c08c17456b942c5a34f0e7bfeaacd1d9343c8d5
Sara <122972681+Sarajona@users.noreply.github.com>
2025-09-24 15:47:35 +0200

    New exercises finished in lesson 5

--- Övning.py
+++ Övning.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-class Car:
+
+#KLASSER OCH OBJEKT
+class car:
     def __init__(self, brand, year):
         self.brand = brand
         self.year = year
@@ -14,10 +16,45 @@ class Car:
         print(self.current_year - self.year)
     
 
-my_car = Car("Toyota Previa", 1995)
+my_car = car("Toyota Previa", 1995)
 my_car.desription()
 my_car.age()
 
-your_car = Car("Lexus 1A", 1975)
+your_car = car("Lexus 1A", 1975)
 your_car.desription()
 your_car.age()
+
+print("\n-------------------\n")
+
+# Arv och polymorfism #
+class Vehicle:
+    def __init__(self, brand):    
+        self.brand = brand
+    def drive(self):
+        print("I'm a vehicle and I'm driving!")
+
+class Car(Vehicle):
+    def drive(self):
+        print("Vroom vroom I'm a car and I am driving")
+
+class Bicycle(Vehicle):
+    def drive(self):
+        print("Pling pling I'm a bicycle and I am cycling")
+
+class Boat(Vehicle):
+    def drive(self):
+        print("Hooonk Hooonk I'm a boat and I'm floating")
+       
+bicycle_one = Bicycle("Monark")
+car_one = Car("Tesla")
+boat_one = Boat("Candela")
+
+bicycle_one.drive()
+car_one.drive()
+boat_one.drive()
+print("\n------------------\n")
+list_of_vehicles = [Car("Tesla"), Bicycle("Monark"), Boat("Candela")]
+for v in list_of_vehicles:
+    v.drive()
+
+print("\n------------------\n")
